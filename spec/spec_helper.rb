2553962cb3930b6cea818910b63dd2b2bd0f5a91
fred mac <mcfredmac@icloud.com>
2014-03-24 23:25:12 +0000

    added spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,21 @@ ENV["RACK_ENV"] = 'test'
 #using the right database for tests
 
 require 'server' 
+require 'database_cleaner' #required so the database is cleaned after every test 
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
+end
