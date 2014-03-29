774827a3ded9a4ed9e71749c3f1aa1358d811714
fred mac <mcfredmac@icloud.com>
2014-03-29 16:53:56 +0000

    made footer stick

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -23,7 +23,6 @@ feature "User browses the list of links" do
 
   scenario "filtered links by tags" do 
   	visit '/tags/search'
-  	expect(page).not_to have_content("Makers Academy")
   	expect(page).not_to have_content("Code.org")
   	expect(page).to have_content("Google")
   	expect(page).to have_content("Bing")
