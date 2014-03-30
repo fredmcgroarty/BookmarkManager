051b337747a575c7f8b229ecb3f4b784ee9d6a01
fred mac <mcfredmac@icloud.com>
2014-03-31 00:05:50 +0100

    data enters db, now write test to show data is displayed on screen

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,19 +1,19 @@
-	get '/' do
-	  @links = Link.all
-	  erb :"index"
-	end
+get '/' do
+  @links = Link.all
+  erb :"index"
+end
+get '/links/new' do
+	erb :"links/new"
+end
 
-	get '/links/new' do
-		erb :"links/new"
-	end
-
-	post '/links' do
+post '/links' do 
 		url = params["url"]
 		title = params["title"]
+		time = Time.now
+		instance = time.strftime("%Y-%m-%d %T")
 		tags = params["tags"].split(" ").map do |tag|
     	Tag.first_or_create(:text => tag)
   	end
-		Link.create(:url => url, :title => title, :tags => tags)
+		Link.create(:url => url, :title => title, :tags => tags, :instance => instance)
 		redirect to('/')
 	end
-
--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,7 @@ class Link
 	property :id,	Serial 
 	property :title, String 
 	property :url, String 
+	property :instance, String
 	has n, :tags, :through => Resource
 
 end
