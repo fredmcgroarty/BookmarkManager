5592d826d7d6b6af77d3e1c07ec623f2a6535f74
fred mac <mcfredmac@icloud.com>
2014-03-30 23:54:35 +0100

    fixed tests, now add link time stamp tests

--- spec/features/addling_links_spec.rb
+++ spec/features/addling_links_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 	feature "User adds a new link" do 
 		scenario "when browsing the homepage" do 
 			expect(Link.count).to eq(0)
-			visit '/links/new'
-			add_link("http://www.makersacademy.com/", "Makers Academy")
+			visit "/links/new"
+			add_link("http://www.makersacademy.com/", 
+                "Makers Academy", 
+                ['education', 'ruby'])
 			expect(Link.count).to eq(1)
 			link = Link.first 
 			expect(link.url).to eq("http://www.makersacademy.com/")
@@ -22,7 +24,7 @@ require 'spec_helper'
   end
 
   def add_link(url, title, tags = [])
-		within("#new-link") do 
+		within ('#container') do 
 			fill_in 'url', :with => url 
 			fill_in 'title', :with => title 
 		 	fill_in 'tags', :with => tags.join(' ')
