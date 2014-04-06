d84ace1701a6cb8933204d02070f7686239904a0
fred mac <mcfredmac@icloud.com>
2014-04-06 18:32:40 +0100

    improved tests

--- spec/features/user_features.rb
+++ spec/features/user_features.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include SessionHelper
 
 feature "User can have a profile" do
 
@@ -6,16 +7,22 @@ feature "User can have a profile" do
     User.create(:email => "test@test.com",
                 :password => 'test',
                 :password_confirmation => 'test')
+    User.create(:email => "othertest@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
     Link.create(:url => "http://www.makersacademy.com",
                 :title => "Makers Academy", 
-                :tags => [Tag.first_or_create(:text => 'education')])
-                :description => "xxxxxx"
+                :tags => [Tag.first_or_create(:text => 'education')],
+                :description => "xxxxxx")
   }
 
   scenario "and see their page with links" do 
   	visit '/'
   	sign_in('test@test.com', 'test')
-  	visit '/profile'
+  	visit '/profile/3'
   	expect(page).to have_content("Makers Academy")
+
   end
+
+end
 
---
+++ spec/features/user_features_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+include SessionHelper
+
+feature "User can have a profile" do
+
+	before(:each) {
+    User.create(:id => "1",
+                :email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+    User.create(:email => "othertest@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+    Link.create(:url => "http://www.Jamesbrown.com",
+                :title => "James Brown", 
+                :tags => [Tag.first_or_create(:text => 'education')],
+                :description => "xxxxxx")
+  }
+
+  scenario "and see their page with links" do 
+  	visit '/'
+  	sign_in('test@test.com', 'test')
+    visit "/links/new"
+    add_link("www.jamesbrown.com", 
+                "James Brown", 
+                ['music', 'funk', 'legend'])
+  	visit '/profile/1'
+  	expect(page).to have_content("James Brown")
+    visit '/profile/2'
+    expect(page).not_to have_content("James Brown")
+
+
+  end
+
+  def add_link(url, title, tags = [])
+    within ('#container') do 
+      fill_in 'url', :with => url 
+      fill_in 'title', :with => title 
+      fill_in 'tags', :with => tags.join(' ')
+      click_button 'Add link'
+    end
+  end 
+
+end
+
